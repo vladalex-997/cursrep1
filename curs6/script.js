da6de1f319a1c9393cb32dba0fd2be6922b19ada
Vlad Alex <vladalex997@gmail.com>
2024-10-29 21:48:19 +0200

    bla

---
+++ curs6/script.js
@@ -0,0 +1,12 @@
+console.log("Bla");
+
+// alert("gigel");
+
+//pentru a lua un element din html dupa id
+document.getElementById("hello").innerText = "Hello";
+
+// var , let , const -> cele 3 tipuri de variabile in JS , se foloseste let si const
+
+//let poate sa primeasca alta valoare, const ramane constanta
+
+let myvariable = "Text";
